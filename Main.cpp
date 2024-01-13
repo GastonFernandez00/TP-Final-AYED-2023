e2add5406e5b531c64e005716cc087de90dfb164
Gaston <gastonfernandez.00@outlook.com>
2024-01-12 21:18:11 -0300

    funciona el main fuera de includes, por lo pronto

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Terminal.h"
+#include "./Includes/Terminal.h"
 using namespace std;
 
 int crearPaginas();
@@ -10,16 +10,19 @@ int main(int argc, char const *argv[])
     Pagina p[tamaniop];
 
     for(int i = 0; i < tamaniop; i++){
-        cout<<p[i].getTamanio()<<endl;
+        cout<<"Lista N"<<i+1<<" -> "<<p[i].getTamanio()<<endl;
     }
-
-
+   
     return 0;
 }
 int crearPaginas(){
-    int i = 1;
+    int i=-1;
     cout<<"Cuantas paginas se quieren enviar por PC?"
         <<"(El maximo es 256)\nIngresar cantidad: \n";
-    cin>>i;
+        cin>>i;
+    while( i<1 || i>256 ){
+        cout<<"\nERROR: valores entre 1 y 256\n";
+        cout<<"Reingresar: ";cin>>i;
+    }
     return i;
 }
